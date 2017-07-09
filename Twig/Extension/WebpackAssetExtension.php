606c325e71cb184b1ce29cee3153808cb24ca9d2
vidandev <vidandev@gmail.com>
2017-07-09 20:39:59 +0200

    Fixed configuration and url generation with package bug.

--- Twig/Extension/WebpackAssetExtension.php
+++ Twig/Extension/WebpackAssetExtension.php
@@ -3,6 +3,7 @@
 namespace Vidandev\WebpackAssetsBundle\Twig\Extension;
 
 use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -40,27 +41,65 @@ class WebpackAssetExtension extends AbstractExtension
 
     /**
      * @param $path
-     * @param bool $live
      * @param $packageName
+     * @param bool $liveEnabled
      * @return string
      */
-    public function getWebpackAssetUrl($path, $live = true, $packageName = null)
+    public function getWebpackAssetUrl($path, $packageName = null, $liveEnabled = true)
     {
-        $devServerConfig = $this->config['dev_server'];
-
-        if ((false === $live) || (false === $devServerConfig['enabled'])) {
+        if (!$this->isLiveAssetLoadingEnabled($liveEnabled)){
             return $this->assetExtension->getAssetUrl($path, $packageName);
         }
 
-        $liveAssetUrl = $devServerConfig['host'];
+        return $this->generateLiveAssetUrl($path, $packageName);
+    }
 
-        if ($port = $devServerConfig['port']) {
-            $liveAssetUrl .= (':' . $port);
+    /**
+     * @param bool $liveEnabled
+     * @return bool
+     * @throws \Exception
+     */
+    private function isLiveAssetLoadingEnabled($liveEnabled)
+    {
+        if (!array_key_exists('dev_server', $this->config)) {
+            throw new \Exception(sprintf('VidandevWebpackAssetsBundle is not configured properly. 
+            No dev_server key found in configuration. %s - line: %s', __METHOD__, __LINE__));
         }
 
-        $subPath = $this->assetExtension->getAssetUrl($path, $packageName);
-        $preparedPath = substr($subPath, strpos($subPath, 'web/'));
+        return ((true === $liveEnabled) && ($this->config['dev_server']['enabled']));
+    }
 
-        return $liveAssetUrl . '/' .$preparedPath;
+    private function generateLiveAssetUrl($path, $packageName = null)
+    {
+        $devServerConfig = $this->config['dev_server'];
+
+        $liveAssetUrl = [];
+
+        $host = $devServerConfig['host'];
+        $port = $devServerConfig['port'];
+
+        $liveAssetUrl[] = $host;
+
+        if ($port && $host) {
+            $liveAssetUrl[] = ':';
+            $liveAssetUrl[] = $port;
+        }
+
+        $liveAssetUrl[] = '/';
+
+        $subPath = $this->assetExtension->getAssetUrl($path, $packageName);
+        $liveAssetUrl[] = substr($subPath, strpos($subPath, 'web/'));
+
+        return implode("", $liveAssetUrl);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'webpack_asset';
     }
 }
